fff6110e29241ed3ba14f09b5b604cc869502503
Aliseiko <aliseiko.dev@gmail.com>
2024-01-02 23:15:16 +0300

    feat: 46 passing, 3 pending

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -496,8 +496,10 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  arr.sort((a, b) => b - a);
+  const maxLength = n > arr.length ? arr.length : n;
+  return arr.slice(0, maxLength);
 }
 
 /**
